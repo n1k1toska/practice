77045d5c9ac2092b4e01e6ee9d1d101555599159
n1k1toska <qwertynick657@gmail.com>
2026-03-16 11:07:09 +0300

    few changes

--- app/src/main/java/com/example/myapplication/FragmentsActivity.java
+++ app/src/main/java/com/example/myapplication/FragmentsActivity.java
@@ -6,24 +6,39 @@ import androidx.activity.EdgeToEdge;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class FragmentsActivity extends AppCompatActivity {
+
+    private FrameLayout fragmentContainer;
+    private Button btnFragment1, btnFragment2, btnFragment3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_fragments);
 
+        fragmentContainer = findViewById(R.id.fragmentContainer);
+        btnFragment1 = findViewById(R.id.btnFragment1);
+        btnFragment2 = findViewById(R.id.btnFragment2);
+        btnFragment3 = findViewById(R.id.btnFragment3);
+
+        loadFragment(new StaticFragment());
+
+        // Обработчики кнопок
+        btnFragment1.setOnClickListener(v -> loadFragment(new StaticFragment()));
+        btnFragment2.setOnClickListener(v -> loadFragment(new DynamicFragment()));
+        btnFragment3.setOnClickListener(v -> loadFragment(new ContainerViewFragment()));
+    }
+
+    private void loadFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        DynamicFragment dynamicFragment = new DynamicFragment();
-        fragmentTransaction.add(R.id.dynamicFragmentContainer, dynamicFragment);
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+        fragmentTransaction.addToBackStack(null); // Добавляем в стек для кнопки "Назад"
         fragmentTransaction.commit();
-
-        FragmentTransaction transaction2 = fragmentManager.beginTransaction();
-        ContainerViewFragment containerViewFragment = new ContainerViewFragment();
-        transaction2.add(R.id.fragmentContainerView, containerViewFragment);
-        transaction2.commit();
     }
 }
